ceefd364990c8d2f4ca94df64dee565526a24285
ndukwesamuel23 <ndukwesamuel23@gmail.com>
2020-09-26 12:18:54 +0100

    my first version

--- dev_to/settings.py
+++ dev_to/settings.py
@@ -126,7 +126,11 @@ USE_TZ = True
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_URL ='/Images/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
